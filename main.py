2ad74982e12ef2a47412b93b51568a6e9b735b89
Joshua Azevedo <joshuaazev@gmail.com>
2023-09-23 10:51:24 -0300

    cubesssss

--- main.py
+++ main.py
@@ -72,15 +72,21 @@ def predict_cube(cap, model):
 
     original_frame = frame.copy()
 
+    good_results = {}
+
     results = model(frame)[0]
     name = None
     for result in results.boxes.data.tolist():
         x1, y1, x2, y2, score, class_id = result
         if score > threshold:
             name = model.names[int(class_id)].upper()
+            good_results[name] = y2
             cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 4)
             cv2.putText(frame, "{} {:.2f}".format(name, score), (int(x1), int(y1 - 10)),
                             cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3, cv2.LINE_AA)
+        
+    if good_results:
+        name = max(good_results, key=good_results.get)
     # cv2.imshow("foto", frame)
     random_name = random_hex_name()
     save_path = os.path.join("images_predict", f"{random_name}.png")
@@ -94,11 +100,12 @@ def predict_cube(cap, model):
 
 
 
-ser = serial.Serial("/dev/ttyACM0", 9600, timeout = 1)
 cap = cv2.VideoCapture(0)
 model_cubes = YOLO('cubes.pt')
 
 
+
+ser = serial.Serial("/dev/ttyACM0", 9600, timeout = 1)
 while True:
     try:
         data = ser.read()
